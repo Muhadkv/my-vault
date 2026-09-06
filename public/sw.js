fe27aaa23a206a4a54259bc361507dc7120c68da
Muhadkv <132655660+Muhadkv@users.noreply.github.com>
2026-09-06 21:13:52 +0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vault-cache-v1'
+const CACHE_NAME = 'vault-cache-v2'
 
 self.addEventListener('install', (event) => {
   self.skipWaiting()
@@ -6,7 +6,9 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   )
 })
 
